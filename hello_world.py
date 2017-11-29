b67e3ac65958870f81d5012cd7586ee359c0d8c2
Curtis Huo <2531495316@qq.com>
2017-11-29 22:01:25 +0800

    changed hello world to include student class

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,20 @@
 name = "Curtis"
-print("Hello world, my name is " + Curtis + " this is my first github push!")
+print("Hello world, my name is " + name + " this is my first github push!")
 print(" I am learning computer science. I am excited to see where this journey takes me! Here we go!")
+
+
+class Student(object):
+	def __init__(self, name, age):
+		self.age = age
+		self.name = name
+
+	
+
+c = Student(name="Curtis", age=20)
+print(c.name)
+print(c.age)
+print(type(c))
+
+s2 = Student(name="Kira", age=14)
+print(s2.name)
+print(s2.age)
