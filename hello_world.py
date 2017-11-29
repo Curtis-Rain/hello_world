eaa13257037c6823d100b87d041b0d9884496081
Curtis Huo <2531495316@qq.com>
2017-11-29 21:55:04 +0800

    fist commit

---
+++ hello_world.py
@@ -0,0 +1,3 @@
+name = "Curtis"
+print("Hello world, my name is " + Curtis + " this is my first github push!")
+print(" I am learning computer science. I am excited to see where this journey takes me! Here we go!")
